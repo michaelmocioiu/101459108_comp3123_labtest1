58cbd032217766f560d091fb3b222bdf388ded29
michaelmocioiu <michaelmocioiu@gmail.com>
2024-10-03 16:20:47 -0400

    Updated Q1 Documentation

--- question-1/main.js
+++ question-1/main.js
@@ -1,17 +1,19 @@
 const lowerCaseWords = (mixedArr) => {
     return new Promise((resolve, reject) => {
-        //The array is first fil
+        //The array is first filtered to remove non strings
         const stringArr = mixedArr.filter(x => typeof x === 'string');
+        //if the resulting array is empty, reject promise and return error
         if (!stringArr.length) return reject(new Error("Array does not contain any strings!"));
-        const lowerArr = stringArr.map(x => x.toLowerCase());
-        resolve(lowerArr);
+        //otherwise, resolve and return the array with all strings in lowercase
+        resolve(stringArr.map(x => x.toLowerCase()));
     })
 }
 
-const mixedArr = ['PIZZA', 10, true, 25, false, "Wings"]
-const mixedArr2 = [3, 5, ["a"], false]
 
 //Test case from assignment sheet
+const mixedArr = ['PIZZA', 10, true, 25, false, "Wings"]
 lowerCaseWords(mixedArr).then(result => console.log(result)).catch(error => console.log(error.message))
+
 //Test case where no strings are in the array
+const mixedArr2 = [3, 5, ["a"], false]
 lowerCaseWords(mixedArr2).then(result => console.log(result)).catch(error => console.log(error.message))
